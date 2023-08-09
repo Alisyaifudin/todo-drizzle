e20647052bb970a9520824dd3bbc93ed7a4ce605
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2023-08-09 22:23:45 +0700

    functional todo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,96 @@
-import { db } from "@/db"
-import { tasks } from "@/db/schema"
+"use client";
 
-export default async function Home() {
-  const result = await db.select().from(tasks);
-  return (
-    <main>
-      <h1>To Do List</h1>
+import { useState } from "react";
 
-    </main>
-  )
+// import { db } from "@/db";
+// import { tasks } from "@/db/schema";
+
+const originalTasks = [
+	{
+		id: 1,
+		name: "Task 1",
+		completed: false,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+	{
+		id: 2,
+		name: "Task 2",
+		completed: false,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+];
+
+export default function Home() {
+	const [tasks, setTasks] = useState(originalTasks);
+	const handleCheck = (id: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newTasks = tasks.map((task) => {
+			if (task.id === id) {
+				task.completed = e.target.checked;
+			}
+			return task;
+		});
+		setTasks(newTasks);
+	};
+	const handleDelete = (id: number) => () => {
+		const newTasks = tasks.filter((task) => task.id !== id);
+		setTasks(newTasks);
+	};
+
+	const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const form = e.currentTarget;
+		const data = new FormData(form);
+		const name = data.get("task") as string;
+		const ids = tasks.length > 0 ? tasks.map((task) => task.id) : [0];
+		const maxId = Math.max(...ids);
+		const newTasks = [
+			...tasks,
+			{
+				id: maxId + 1,
+				name,
+				completed: false,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+		];
+		setTasks(newTasks);
+	};
+
+	return (
+		<main className="flex m-10 flex-col gap-3">
+			<h1 className="text-xl font-bold">To Do List</h1>
+			<form onSubmit={handleAdd} className="flex gap-1 justify-between w-full">
+				<input
+					name="task"
+					className="flex-1 px-1 text-black border-2 rounded-md border-gray-500"
+					type="text"
+				/>
+				<button className="bg-blue-500 text-white rounded-md px-3 py-1">Add</button>
+			</form>
+			<ul className="flex flex-col gap-1 w-full">
+				{tasks.map((task) => (
+					<li key={task.id} className="flex gap-1 justify-between w-full">
+						<label
+							htmlFor={"task" + task.id}
+							className={`flex-1 ${task.completed ? "line-through" : null}`}
+						>
+							{task.name}
+						</label>
+						<input
+							onChange={handleCheck(task.id)}
+							className="hidden"
+							id={"task" + task.id}
+							name="task"
+							type="checkbox"
+						/>
+						<button onClick={handleDelete(task.id)} className="text-white rounded-md">
+							🗑️
+						</button>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,12 @@
 import { InferModel } from "drizzle-orm";
-import { date, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
- 
-export const tasks = pgTable('tasks', {
-    id: serial("id").primaryKey(),
-    title: varchar("title", { length: 255 }).notNull(),
-    description: text("description"),
-    completed: text("completed").notNull().default("false"),
-    createdAt: date("created_at", { mode: "date"}).defaultNow().notNull(),
-    updatedAt: date("updated_at", { mode: "date"}).defaultNow().notNull()
+import { boolean, date, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+
+export const tasks = pgTable("tasks", {
+	id: serial("id").primaryKey(),
+	name: varchar("name", { length: 255 }).notNull(),
+	completed: boolean("completed").default(false).notNull(),
+	createdAt: date("created_at", { mode: "date" }).defaultNow().notNull(),
+	updatedAt: date("updated_at", { mode: "date" }).defaultNow().notNull(),
 });
 
 type Task = InferModel<typeof tasks, "select">;
